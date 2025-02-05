b5b998b7ac02412eccfbcedfd89cdebed2b8d251
Hrasek <hosekmotor@seznam.cz>
2025-02-05 15:10:16 +0100

    feat: And a bit more of restructuring.

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { CORE_CONCEPTS, EXAMPLES } from './../data.js';
 import TabButton from './TabButton.jsx';
 import Section from './Section.jsx';
+import Tabs from './Tabs.jsx';
 
 export default function Examples() {
 
@@ -12,21 +13,21 @@ export default function Examples() {
     } 
     return (
         <Section title="Examples" id="examples">
-          <menu>
-            {CORE_CONCEPTS.map((concept) => (
+            <Tabs buttons={CORE_CONCEPTS.map((concept) => (
               <TabButton isSelected={selectedTopic === concept.title.toLowerCase()} onClick={() => handleSelect(concept.title)} key={concept.title}>{concept.title}</TabButton>
             )
-            )}
-          </menu>
-          {selectedTopic ? (
-            <div id="tab-content">  
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>) : (<p>Please select a topic.</p>)
-          }
+            )}>
+                {selectedTopic ? (
+                <div id="tab-content">  
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+                </div>) : (<p>Please select a topic.</p>)
+                }
+            </Tabs> 
+
         </Section>  
     );
 }   
